716316381fb6223bffaadc47b9ada9861ba0d196
vien pham <vienpham@viens-Air.home>
2023-08-15 13:03:32 -0500

    final commit for rating content

--- workshop/interactive-rating-component-main/main.js
+++ workshop/interactive-rating-component-main/main.js
@@ -1,6 +1,23 @@
 const card = document.querySelector("#card");
 const button = document.querySelector("#button");
+const numbers = document.getElementsByClassName("card__front-number");
+const ratting = document.querySelector("#card__back-ratting");
 
+let ratting_num = 5;
 button.addEventListener("click", () => {
-    card.dataset.display = "back"
+    card.dataset.display = "back";
+    ratting.innerHTML = `You selected ${ratting_num} out of 5`;
 })
+
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", () => {
+        ratting_num = parseInt(numbers[i].dataset.ratingvalue);
+        for (let i = 0; i < numbers.length; i++) {
+            numbers[i].dataset.select = ratting_num == numbers[i].dataset.ratingvalue;
+
+        }
+    })
+};
+
+
